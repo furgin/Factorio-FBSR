c58f2e810b4349ca8da6f762f567e5fa97986f99
Matthew Jensen <mjensen@atlassian.com>
2020-02-12 16:05:07 +1100

    0.6

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION=0.6
+
+ID=`docker images registry.furgin.org/furgin/factorio-fbsr:$VERSION --format "{{.ID}}"`
+
+if [ ! -z "$ID" ]; then
+    echo "Docker tag for version $VERSION already exists with ID: $ID"
+    exit
+fi
+
+DIR=`pwd`
+cd $DIR/FactorioBlueprintStringRenderer
+mvn clean package
+cd $DIR
+
+docker build -t factorio-fbsr .
+docker tag factorio-fbsr registry.furgin.org/furgin/factorio-fbsr:$VERSION
+docker tag factorio-fbsr registry.furgin.org/furgin/factorio-fbsr:latest
+
+docker push registry.furgin.org/furgin/factorio-fbsr:$VERSION
+docker push registry.furgin.org/furgin/factorio-fbsr:latest
