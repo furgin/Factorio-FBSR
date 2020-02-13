ce89407183ca18d07c18ccaf9ffaaaa0c0ee890d
Matthew Jensen <mjensen@atlassian.com>
2020-02-13 13:06:29 +1100

    icons

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/app/ImageService.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/app/ImageService.java
@@ -18,9 +18,12 @@ import org.rapidoid.setup.App;
 import org.rapidoid.setup.Setup;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -43,6 +46,7 @@ public class ImageService extends AbstractIdleService {
         String address = configJson.optString("bind", "0.0.0.0");
         int port = configJson.optInt("port", 80);
         File folder = new File(configJson.optString("local-storage"));
+        File factorioFolder = new File(Config.get().optString("factorio") + "/data/base/graphics/icons/");
 
         Setup images = HttpServers.create(address, port);
 
@@ -51,6 +55,32 @@ public class ImageService extends AbstractIdleService {
             resp.contentType(MediaType.IMAGE_PNG);
             resp.header("Cache-Control", "max-age=31556926, public");
             resp.file(new File(folder, req.param("file")));
+
+            return resp;
+        });
+
+        images.get("/icons/{file}").serve((req, resp) -> {
+
+            File iconFile = new File(factorioFolder, req.param("file"));
+            if (iconFile.exists()) {
+                BufferedImage image = ImageIO.read(iconFile);
+                Image iconImage = (BufferedImage) image
+                        .getSubimage(0, 0, image.getHeight(), image.getHeight());
+                BufferedImage finalImage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g2 = (Graphics2D) finalImage.getGraphics();
+                g2.drawImage(iconImage, 0, 0, 32, 32, null);
+                g2.dispose();
+
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                ImageIO.write(finalImage, "png", buffer);
+
+                resp.contentType(MediaType.IMAGE_PNG);
+                resp.header("Cache-Control", "max-age=31556926, public");
+                resp.body(buffer.toByteArray());
+            } else {
+                resp.body("Not Found".getBytes());
+                resp.code(404);
+            }
 
             return resp;
         });
